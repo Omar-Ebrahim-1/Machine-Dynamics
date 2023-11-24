7b2539d932a54b6c03d81d9e2e5592c2cbbe969d
Omar <ebrahimo@uwindsor.ca>
2023-11-24 12:48:19 -0500

    Acceleration

--- Machine_Project.m
+++ Machine_Project.m
@@ -2,16 +2,30 @@
 clear; clc;
 
 % --------------------------------------------
-% Calculate the velocity and acceleration of A
+% Calculate the velocity of point A
 % --------------------------------------------
 % Define symbolic variables
 syms R theta omega t
 
 % Define position vector
-r_a = R * [-sin(theta), cos(theta)];
+r_atheta = R * [-sin(theta), cos(theta)];
 % Calculate velocity vector from r*omega
-v_a = r_a .* omega;
+v_a = r_atheta .* omega;
 
 % Extract components
 v_ax = v_a(1)
 v_ay = v_a(2)
+
+% --------------------------------------------
+% Calculate the acceleration of point A
+% --------------------------------------------
+
+% Define position vector
+r_ao = R * [cos(theta), sin(theta)];
+
+% Calculate acceleration vector from -omega^2 * r
+a_a = -omega^2 * r_ao;
+
+% Extract components
+a_ax = a_a(1)
+a_ay = a_a(2)
