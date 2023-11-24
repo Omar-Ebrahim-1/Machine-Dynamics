e51cf124f4fd2c211527c702857c1d260493fb68
Omar <ebrahimo@uwindsor.ca>
2023-11-24 13:05:44 -0500

    progress?

--- Machine_Project.m
+++ Machine_Project.m
@@ -4,17 +4,17 @@ clear; clc;
 % --------------------------------------------
 % Calculate the velocity of point A
 % --------------------------------------------
+
 % Define symbolic variables
-syms R theta omega t
+syms L R theta omega alpha
 
 % Define position vector
 r_atheta = R * [-sin(theta), cos(theta)];
+
 % Calculate velocity vector from r*omega
 v_a = r_atheta .* omega;
-
-% Extract components
-v_ax = v_a(1)
-v_ay = v_a(2)
+v_ax = v_a(1);
+v_ay = v_a(2);
 
 % --------------------------------------------
 % Calculate the acceleration of point A
@@ -25,7 +25,34 @@ r_ao = R * [cos(theta), sin(theta)];
 
 % Calculate acceleration vector from -omega^2 * r
 a_a = -omega^2 * r_ao;
+a_ax = a_a(1);
+a_ay = a_a(2);
 
-% Extract components
-a_ax = a_a(1)
-a_ay = a_a(2)
+% --------------------------------------------
+% Calculate the velocities of the crank AC
+% --------------------------------------------
+
+% Define position vector
+r_ca = L * [-sin(theta), cos(theta)];
+
+% Calculate velocity vector from v_a + r*omega
+v_c = v_a + omega * r_ca;
+v_cx = v_c(1);
+v_cy = v_c(2);
+
+% Calculate the angular velocity of the crank from omega = v/r
+omega_c = -v_ay / r_ca(2);
+
+% --------------------------------------------
+% Calculate the accelerations of the crank AC
+% --------------------------------------------
+
+% Calculate acceleration vector from a_a + alpha * r - omega^2 * r
+a_c = a_a + alpha * r_ca - omega^2 * r_ca;
+a_cx = a_c(1);
+a_cy = a_c(2);
+
+% Calculate the acceleration of the mass center of the crank AV
+a_g_cx = a_ax + alpha * r_ca(1)/2 - omega^2 * r_ca(1);
+a_g_cy = a_ay + alpha * r_ca(2)/2 - omega^2 * r_ca(2);
+
