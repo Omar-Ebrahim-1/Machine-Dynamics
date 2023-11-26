7c7970f20721a632246ab8f6cd82a03ffd873e62
__OmarEbrahim__ <omarkhaled200321@gmail.com>
2023-11-26 13:11:05 -0500

    Finished??

--- Machine_Project.m
+++ Machine_Project.m
@@ -13,15 +13,13 @@ clear; clc;
 
 % Define symbolic variables
 syms L R theta omega alpha m_c r_c phi
-syms v_a_y a_a_y
+syms v_a_y a_a_x a_a_y
 
 % Define position vector
 r_atheta = R * [-sin(theta), cos(theta)];
 
 % Calculate velocity vector from r*omega
 v_a = r_atheta .* omega;
-v_ax = v_a(1);
-v_ay = v_a(2);
 
 % --------------------------------------------
 % Calculate the acceleration of point A
@@ -58,8 +56,6 @@ alpha_c = -(a_a_y + omega^2 * L * sin(phi)) / L * cos(phi);
 
 % Calculate acceleration vector from a_a + alpha * r - omega^2 * r
 a_c = a_a + alpha * r_ca - omega^2 * r_ca;
-a_cx = a_c(1);
-a_cy = a_c(2);
 
 % Calculate the acceleration of the mass center of the crank AC
 a_g_cx = -(omega^2 * R * cos(theta))/2;
@@ -91,8 +87,6 @@ alpha_b = -(a_a_y + omega^2 * L * sin(phi)) / L*cos(phi);
 
 % Calculate acceleration vector from a_a + alpha * r - omega^2 * r
 a_b = a_a + alpha * r_ba - omega^2 * r_ba;
-a_bx = a_b(1);
-a_by = a_b(2);
 
 % Calculate the acceleration of the mass center of the connecting rod AB
 a_g_bx = a_ax + alpha * r_ba(1)/2 - omega^2 * r_ba(1);
@@ -131,31 +125,25 @@ theta1 = omega*x; % rad
 % --------------------------------------------------------
 
 % Calculate the angle phi
-phi = matlabFunction(phi); % rad
-phi = phi(L, R, theta1); % rad
+phi = asin(R/L .* sin(theta1)); % rad
 
 % Calculate the velocity of point A
-v_ax = matlabFunction(v_ax); % m/s
+v_ax = @(R, omega, theta1) ...
+  R .* omega .* cos(theta1); % m/s
 v_ax = v_ax(R, omega, theta1); % m/s
 
-v_ay = matlabFunction(v_ay); % m/s
+v_ay = @(R, omega, theta1) ...
+  R .* omega .* sin(theta1); % m/s
 v_ay = v_ay(R, omega, theta1); % m/s
 
 % Calculate the acceleration of point A
-a_ax = matlabFunction(a_ax); % m/s^2
-a_ax = a_ax(R, omega, theta1); % m/s^2
+a_ax = @(R, omega, theta1) ...
+  -R .* omega.^2 .* cos(theta1); % m/s.^2
+a_ax = a_ax(R, omega, theta1); % m/s.^2
 
-a_ay = matlabFunction(a_ay); % m/s^2
-a_ay = a_ay(R, omega, theta1); % m/s^2
-
-% Calculate the acceleration of point B
-a_bx = @(a_ax, alpha_b, omega_b, L, phi) ...
-  a_ax + alpha_b * L * sin(phi) - omega_b^2 * L * cos(phi); % m/s^2
-a_bx = a_bx(a_ax, alpha_b, omega_b, L, phi); % m/s^2
-
-a_by = @(a_ay, alpha_b, omega_b, L, phi) ...
-  a_ay + alpha_b * L * cos(phi) - omega_b^2 * L * sin(phi); % m/s^2
-a_by = a_by(a_ay, alpha_b, omega_b, L, phi); % m/s^2
+a_ay = @(R, omega, theta1) ...
+  -R .* omega.^2 .* sin(theta1); % m/s.^2
+a_ay = a_ay(R, omega, theta1); % m/s.^2
 
 % Calculate the angular velocity and acceleration of the crank
 omega_c = matlabFunction(omega_c); % rad/s
@@ -171,10 +159,23 @@ omega_b = omega_b(L, R, phi, v_ay); % rad/s
 alpha_b = matlabFunction(alpha_b); % rad/s^2
 alpha_b = alpha_b(L, R, a_ay, omega_b, phi); % rad/s^2
 
+% Calculate the acceleration of point B
+a_bx = @(a_ax, alpha_b, omega_b, L, phi) ...
+  a_ax + alpha_b .* L .* sin(phi) - omega_b.^2 .* L .* cos(phi); % m/s.^2
+a_bx = a_bx(a_ax, alpha_b, omega_b, L, phi); % m/s.^2
+
+a_by = @(a_ay, alpha_b, omega_b, L, phi) ...
+  a_ay + alpha_b .* L .* cos(phi) - omega_b.^2 .* L .* sin(phi); % m/s.^2
+a_by = a_by(a_ay, alpha_b, omega_b, L, phi); % m/s.^2
+
 % Calculate the acceleration of the crank
 a_cx = @(a_ax, alpha_c, omega_c, L, phi) ...
   a_ax + alpha_c .* L .* sin(phi) - omega_c.^2 .* L .* cos(phi); % m/s.^2
 a_cx = a_cx(a_ax, alpha_c, omega_c, L, phi); % m/s.^2
+
+a_cy = @(a_ay, alpha_c, omega_c, L, phi) ...
+  a_ay + alpha_c .* L .* cos(phi) - omega_c.^2 .* L .* sin(phi); % m/s.^2
+a_cy = a_cy(a_ay, alpha_c, omega_c, L, phi); % m/s.^2
 
 % Calculate the acceleration of the mass center of the crank
 a_g_cx = matlabFunction(a_g_cx); % m/s.^2
@@ -265,48 +266,45 @@ ylabel('Acceleration (m/s.^2)')
 legend('a_{bx}', 'a_{by}')
 grid on
 
-% Plot the angular velocity of the crank
+
+% Plot the angular velocity and acceleration of the crank
 figure(4)
 plot(x, omega_c)
-title('Angular velocity of the crank')
-xlabel('x (m)')
-ylabel('Angular velocity (rad/s)')
-grid on
-
-% Plot the angular acceleration of the crank
-figure(5)
+hold on
 plot(x, alpha_c)
-title('Angular acceleration of the crank')
-xlabel('x (m)')
-ylabel('Angular acceleration (rad/s.^2)')
-grid on
-
-% Plot the angular velocity of the connecting rod
-figure(6)
-plot(x, omega_b)
-title('Angular velocity of the connecting rod')
+hold off
+title('Angular velocity and acceleration of the crank')
 xlabel('x (m)')
 ylabel('Angular velocity (rad/s)')
+legend('omega_c', 'alpha_c')
 grid on
 
-% Plot the angular acceleration of the connecting rod
-figure(7)
+% Plot the angular velocity and acceleration of the connecting rod
+figure(5)
+plot(x, omega_b)
+hold on
 plot(x, alpha_b)
-title('Angular acceleration of the connecting rod')
+hold off
+title('Angular velocity and acceleration of the connecting rod')
 xlabel('x (m)')
-ylabel('Angular acceleration (rad/s.^2)')
+ylabel('Angular velocity (rad/s)')
+legend('omega_b', 'alpha_b')
 grid on
 
 % Plot the acceleration of the crank
-figure(8)
+figure(6)
 plot(x, a_cx)
+hold on
+plot(x, a_cy)
+hold off
 title('Acceleration of the crank')
 xlabel('x (m)')
 ylabel('Acceleration (m/s.^2)')
+legend('a_{cx}', 'a_{cy}')
 grid on
 
 % Plot the acceleration of the mass center of the crank
-figure(9)
+figure(7)
 plot(x, a_g_cx)
 hold on
 plot(x, a_g_cy)
@@ -318,7 +316,7 @@ legend('a_{gx}', 'a_{gy}')
 grid on
 
 % Plot the acceleration of the mass center of the connecting rod
-figure(10)
+figure(8)
 plot(x, a_G_cx)
 hold on
 plot(x, a_G_cy)
@@ -329,20 +327,8 @@ ylabel('Acceleration (m/s.^2)')
 legend('a_{Gx}', 'a_{Gy}')
 grid on
 
-% Plot the acceleration of the mass center of the connecting rod
-figure(11)
-plot(x, a_G_bx)
-hold on
-plot(x, a_G_by)
-hold off
-title('Acceleration of the mass center of the connecting rod')
-xlabel('x (m)')
-ylabel('Acceleration (m/s.^2)')
-legend('a_{Gx}', 'a_{Gy}')
-grid on
-
 % Plot the crank forces
-figure(12)
+figure(9)
 plot(x, F_cx)
 hold on
 plot(x, F_cy)
@@ -354,7 +340,7 @@ legend('F_{cx}', 'F_{cy}')
 grid on
 
 % Plot the connecting rod forces
-figure(13)
+figure(10)
 plot(x, F_bx)
 hold on
 plot(x, F_by)
@@ -366,9 +352,10 @@ legend('F_{bx}', 'F_{by}')
 grid on
 
 % Plot the torque
-figure(14)
+figure(11)
 plot(x, T)
 title('Torque')
 xlabel('x (m)')
 ylabel('Torque (N.*m)')
+legend('T')
 grid on
